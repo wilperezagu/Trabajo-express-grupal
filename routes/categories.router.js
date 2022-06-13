af54690d9f2c1a73a7a34d45d3025ed7a30ead73
angie <anyisitapte13@gmail.com>
2022-06-12 20:14:15 -0500

    termine de path en categoria y de usuario el delete

--- routes/categories.router.js
+++ routes/categories.router.js
@@ -1,7 +1,19 @@
 const express = require('express');
-const faker = require('faker');
+const CategoryService = require('./../services/categorys.services');
+const validatorHandler = require('./../middlewares/validator.handler');
+const { createCategorytSchema, updateCategorySchema, getCategorySchema } = require('./../schemas/category.schema');
 
 const router = express.Router();
+const service = new CategoryService();
+
+router.get('/', async (req, res) => {
+  const categorys = await service.find();
+  res.json(categorys);
+});
+
+router.get('/filter', (req, res) => {
+  res.send('Soy un filter')
+})
 
 router.get('/categories/:categoryId/products/:productsId', (req, res) => {
   const { categoryId, productsId } = req.params;
@@ -10,5 +22,20 @@ router.get('/categories/:categoryId/products/:productsId', (req, res) => {
     productsId,
   });
 })
+
+router.patch('/:id',
+  validatorHandler(getCategorySchema, 'params'),
+  validatorHandler(updateCategorySchema, 'body'),
+  async (req, res, next) => {
+    try{
+      const { id } = req.params;
+      const body = req.body;
+      const category = await service.update(id, body);
+      res.json(category);
+    } catch (error) {
+      next(error);
+    }
+  })
+
 
 module.exports = router;
--- routes/users.router.js
+++ routes/users.router.js
@@ -1,18 +1,25 @@
 const express = require('express');
+const usersService = require('./../services/users.services');
+const validatorHandler = require('./../middlewares/validator.handler');
+const { createUserSchema, updateUserSchema, getUserSchema } = require('./../schemas/user.schema');
 
 const router = express.Router();
+const service = new usersService();
 
-//QUERY PARAMS
-router.get('/', (req, res) => {
-  const { limit, offset } = req.query;
-  if (limit && offset){
-    res.json({
-      limit,
-      offset
-    })
-  } else {
-    res.send('No hay parametros')
-  }
+router.get('/', async (req, res) => {
+  const users = await service.find();
+  res.json(users);
+});
+
+router.get('/filter', (req, res) => {
+  res.send('Soy un filter')
 })
+
+router.delete('/:id', (req, res) => {
+  const { id } = req.params;
+  const rta = service.delete(id);
+  res.json(rta);
+});
+
 
 module.exports = router;
