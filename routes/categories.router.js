6e1d339f49facc5c21f1ee0ee4377a11e29d987c
Dnaix <dv20345@gmail.com>
2022-06-15 07:48:13 -0500

    metodos post y delete,categorias

--- routes/categories.router.js
+++ routes/categories.router.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const faker = require('faker');
+const validatorHandler = require('../middlewares/validator.handler');
 
 const router = express.Router();
 
@@ -10,5 +11,18 @@ router.get('/categories/:categoryId/products/:productsId', (req, res) => {
     productsId,
   });
 })
+router.post('/',
+ validatorHandler(createcategorisSchema, 'body'),
+ async (req,res)=> {
+   const body = req.body
+   const newCategory =await ServiceWorker.create(body);
+   res.status(201).json(newCategory)
 
+ })
+
+ router.delete('/:id', (req, res)=>{
+   const { id } = req.params;
+   const rta = service.delete(id);
+   res.json(rta);
+ });
 module.exports = router;
